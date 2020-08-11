c17beba27ab39d522ddddaee093e33b8f49bcbea
Matthias von Ameln <matthias-von-ameln@gmx.de>
2020-08-11 21:11:19 +0200

    output beautyfing deactive verilator tracing

--- tb/core/custom/src/main.c
+++ tb/core/custom/src/main.c
@@ -3,71 +3,127 @@
 
 #include "vpu_asm_wrapper.h"
 
-#define SIZE 16
+// MAX_SIZE = 2^(STEPS*STEPSIZE
+#define MAX_SIZE 4096
+#define STEPS 6
+#define STEPSIZE 2
 
 //extern int __MY_SECTION_START, __MY_SECTION_END;
 //const uint32_t __attribute__((section (".mySection"))) v_a[SIZE] = { 1, 2, 3, 4};
 
-int32_t a[SIZE];
-int32_t b[SIZE];
-int32_t vpu_res[SIZE];
-int32_t cpu_res[SIZE];
+int32_t a[MAX_SIZE];
+int32_t b[MAX_SIZE];
+int32_t vpu_res[MAX_SIZE];
+int32_t cpu_res[MAX_SIZE];
+int32_t sizes[STEPS];
 
-void print_array(int32_t* m) {
+//-----------------------------------------------------------------------------
+// Plugin Testfunction here
+//-----------------------------------------------------------------------------
+void vpu_test_function(size_t step) {
+    vmac_e32(sizes[step], &a, &b, &vpu_res);
+}
 
-  for (int i = 0; i < SIZE; i++) {
+void cpu_test_function(size_t step) {
+  for (int i = 0; i < sizes[step]; i++) {
+    cpu_res[i] = (a[i] * b[i]) + cpu_res[i];
+  }
+}
+//-----------------------------------------------------------------------------
+
+void print_array(size_t size, int32_t* m) {
+
+  for (int i = 0; i < size; i++) {
     printf("%ld ", m[i]);
   }
   printf("\n");
 }
 
 void init_glob_arrays() {
-  for (int i = 0; i < SIZE; i++) {
-    a[i] = i;
-    b[i] = i + SIZE;
-    vpu_res[i] = i*2;
-    cpu_res[i] = i*2;
+  time_t  t;
+
+  srand((unsigned) time(&t));
+
+  for (int i = 0; i < MAX_SIZE; i++) {
+
+    a[i] = rand();
+    b[i] = rand();
+    vpu_res[i] = rand();
+
+    cpu_res[i] = vpu_res[i];
   }
+}
+
+uint8_t compare_results(size_t step) {
+  uint8_t err = 0;
+  for (int i = 0; i < sizes[step]; i++) {
+    if (cpu_res[i] != vpu_res[i]) {
+      printf("ERROR!\n");
+      printf("vpu_res: \n");
+      print_array(sizes[step], vpu_res);
+      printf("cpu_res: \n");
+      print_array(sizes[step], cpu_res);
+      err = 1;
+      break;
+    }
+  }
+  return err;
 }
 
 int main(int argc, char *argv[])
 {
   int32_t err = 0;
-  int32_t cycles = 0;;
-  int32_t instructions = 0;;
+
+  int32_t vpu_cyc[STEPS]   = {0};
+  int32_t vpu_instr[STEPS] = {0};
+  int32_t cpu_cyc[STEPS]   = {0};
+  int32_t cpu_instr[STEPS] = {0};
+
 
   init_glob_arrays();
 
-  // VPU matrix multiplication
-  reset_performance_counter();
-  vmac_e32(SIZE, &a, &b, &vpu_res);
-  cycles       = get_cycle_count();
-  instructions = get_instr_count();
-  printf("With VPU needed cycles:       %lu. \n", cycles);
-  printf("With VPU needed instructions: %lu. \n", instructions);
-
-  reset_performance_counter();
-  for (int i = 0; i < SIZE; i++) {
-    cpu_res[i] = (a[i] * b[i]) + cpu_res[i];
+  for (int i = 0; i < STEPS; i++) {
+    sizes[i] = 1;
   }
-  cycles       = get_cycle_count();
-  instructions = get_instr_count();
-  printf("With CPU needed cycles:       %lu. \n", cycles);
-  printf("With CPU needed instructions: %lu. \n", instructions);
 
-  for (int i = 0; i < SIZE; i++) {
-    if (cpu_res[i] != vpu_res[i]) {
-      err = 1;
+  for (size_t step = 0; step < STEPS; step++) {
+
+    // calculate current size (power of two)
+    for (int i = 0; i < ((step + 1) * STEPSIZE); i++) {
+      sizes[step] *= 2;
+    }
+
+    // VPU
+    reset_performance_counter();
+    vpu_test_function(step);
+    vpu_cyc[step]   = get_cycle_count();
+    vpu_instr[step] = get_instr_count();
+
+    // CPU only equivalent
+    reset_performance_counter();
+    cpu_test_function(step);
+    cpu_cyc[step]    = get_cycle_count();
+    cpu_instr[step]  = get_instr_count();
+
+    err = compare_results(step);
+    if (err) {
+      break;
     }
   }
 
-  if (err == 1) {
-    printf("vpu_res: \n");
-    print_array(vpu_res);
-    printf("cpu_res: \n");
-    print_array(cpu_res);
+  printf("--------------------------------------------------\n");
+  printf("|                  Results                       |\n");
+  printf("|------------------------------------------------|\n");
+  printf("| Array Size |       VPU       |       CPU       |\n");
+  printf("|------------------------------------------------|\n");
+  printf("|            | CYCLES |  INSTR | CYCLES |  INSTR |\n");
+  printf("|------------------------------------------------|\n");
+  for(int i = 0; i < STEPS; i++) {
+    printf("|      %5ld | %6ld | %6ld | %6ld | %6ld | \n",
+        sizes[i], vpu_cyc[i], vpu_instr[i], cpu_cyc[i], cpu_instr[i]
+        );
   }
-
+  printf("--------------------------------------------------\n");
   return err;
 }
   /*
